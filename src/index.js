98bc59eeaf6bbee061c0708242b4881e3e13e5c1
Celinka Cywińska <celinkacywinska@Celinkas-MacBook-Air.local>
2022-08-19 17:18:22 -0700

    Draft of the action

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
 const core = require('@actions/core');
+const { Octokit } = require("@octokit/rest");
 
 async function run() {
-}
 
+    try {
+
+        const repoOwner = core.getInput('repo-owner');
+        const repoName = core.getInput('repo-name');
+        const personalAcessToken = core.getInput('personal-access-token');
+        let outputMessage = 'Deleted Runners IDs: ';
+
+        // Log in
+        const octokit = new Octokit({
+            auth: personalAcessToken
+        })
+
+        // List self-hosted runners for a repository
+        const runners = (await octokit.request('GET /repos/{owner}/{repo}/actions/runners', {
+            owner: repoOwner,
+            repo: repoName
+        })).runners;
+
+        // Delete a self-hosted runner from a repository
+        for (const runner in runners) {
+            if (runner.status === "offline" ){
+                await octokit.request('DELETE /repos/{owner}/{repo}/actions/runners/{runner_id}', {
+                    owner: repoOwner,
+                    repo: repoName,
+                    runner_id: runner.id
+                })
+    
+                outputMessage += runner.id;
+                outputMessage += ', ';
+            }
+        }
+
+        // simple formating to make it look good: remove the last comma and adding a dot
+        outputMessage = outputMessage.trim().slice(0, -1);
+        outputMessage += '.';
+        core.setOutput("output-message", outputMessage);
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
 run();
